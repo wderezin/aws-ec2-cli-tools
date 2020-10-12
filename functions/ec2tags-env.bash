f7e2bf4531b26965d61030b3efe8992e1d46ce94
Walter Derezinski <walter@daringway.com>
2020-10-12 00:30:39 -0400

    first try

--- functions/ec2tags-env.bash
+++ functions/ec2tags-env.bash
@@ -5,8 +5,7 @@ function ec2tags-env {
   INSTANCE_ID=${INSTANCE_ID:-$(ec2metadata --instance-id)}
 
   # Show Instance Tags as variables
-  eval $(
-    aws ec2 describe-tags --filters "Name=resource-id,Values=${INSTANCE_ID}" --query 'Tags[*].[Key,Value]' --output text | while read KEY VALUE
+  eval $(aws ec2 describe-tags --filters "Name=resource-id,Values=${INSTANCE_ID}" --query 'Tags[*].[Key,Value]' --output text | while read KEY VALUE
     do
       KEY=$(echo ${KEY} |  tr a-z-: A-Z__ )
       echo export ${KEY}=\"${VALUE}\"
