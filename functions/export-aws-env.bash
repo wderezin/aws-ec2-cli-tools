e49037083a1104105fe732d52e9f53f9478427a2
Walter Derezinski <wderezin@users.noreply.github.com>
2020-10-12 08:04:57 -0400

    Init (#1)
    * Initial Release

---
+++ functions/export-aws-env.bash
@@ -0,0 +1,13 @@
+
+function export-aws-env {
+  if [[ "${AWS_DEFAULT_REGION}" == "" ]]
+  then
+    export AWS_DEFAULT_REGION=$(curl -s --connect-timeout 3 -s http://169.254.169.254/latest/dynamic/instance-identity/document/ | jq -r '.region')
+  fi
+
+  while read VAR
+  do
+    eval export $VAR
+  done < <(aws-env-vars)
+}
+export -f export-aws-env
