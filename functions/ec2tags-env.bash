0105bcbbc1cae23b2aa7200e575242e6996c903f
Walter Derezinski <walter@daringway.com>
2020-10-12 05:51:53 -0400

    first try

--- functions/ec2tags-env.bash
+++ functions/ec2tags-env.bash
@@ -1,14 +1,7 @@
 
 function ec2tags-env {
-  setup-aws-environment
-
-  INSTANCE_ID=${INSTANCE_ID:-$(ec2metadata --instance-id)}
-
-  # Show Instance Tags as variables
-  eval $(aws ec2 describe-tags --filters "Name=resource-id,Values=${INSTANCE_ID}" --query 'Tags[*].[Key,Value]' --output text | while read KEY VALUE
-    do
-      KEY=$(echo ${KEY} |  tr a-z-: A-Z__ )
-      echo export ${KEY}=\"${VALUE}\"
-    done
-  )
+  for VAR in $(ec2tags-vars)
+  do
+    eval export $VAR
+  done
 }
